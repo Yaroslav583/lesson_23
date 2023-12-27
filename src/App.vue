7ec155dd81268c59241dd38734f8cadaec0c5c51
Yaroslav <makhinko180601@gmail.com>
2023-12-27 10:34:01 +0100

    Add vue,axios project Lesson_23

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+    }
+
+  },
+  created() {
+    axios. get('https://api.giphy.com/v1/gifs/trending?api_key=xWN5FetwcKhYyWXKJyEjl1Fm1ADLAftr').then((response)=>{
+      console.log(response.data)
+    })
+
+
+  }
+}
+</script>
+
